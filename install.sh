1535da7016c1b056968cf82fe53526ba1abad21e
Sebastian Bellucci <sebastian.bellucci@gmail.com>
2024-12-24 19:34:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ apt update
 apt upgrade -y
 
 # Install Pre-requisite
-apt install -y thunar build-essential git libx11-dev libxft-dev libxinerama-dev xorg dmenu unzip lxpolkit x11-xserver-utils unzip wget pulseaudio alacritty pavucontrol xdg-user-dirs
+apt install -y thunar build-essential git libx11-dev libxft-dev libxinerama-dev xorg dmenu unzip lxpolkit x11-xserver-utils unzip wget pulseaudio alacritty pavucontrol xdg-user-dirs slim
 
 
 # Install Dwm
@@ -25,10 +25,13 @@ cd debian-dwm
 # Making .config and Moving config files and background to Pictures
 mkdir -p /home/$username/.config
 mkdir -p /home/$username/Pictures/Wallpapers
-touch /home/$username/.xinitrc
-cp .xinitrc /home/$username
+#touch /home/$username/.xinitrc
+#cp .xinitrc /home/$username
 cp -R dotfiles/dotconfig/* /home/$username/.config/
-cp dotfiles/Wallpapers/* /home/$username/Pictures/Wallpapers/
+cp dotfiles/dotextras/Wallpapers/* /home/$username/Pictures/Wallpapers/
+cp dotfiles/dotextras/font/ */usr/share/fonts
+cp dotfies/dotextras/slim/slim.conf /etc
+cp -r dotfiles/dotextras/slim/blue-sky /usr/share/slim/themes
 chown -R $username:$username /home/$username
 cd ..
 
