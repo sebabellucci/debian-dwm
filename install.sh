941491a33a2db2bb06cb2495e4d90c4354b5f7ff
Sebastian Bellucci <sebastian.bellucci@gmail.com>
2024-12-24 17:41:35 -0300

    Update install.sh
    fix errors

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-cd /temp
+cd /tmp
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
@@ -15,7 +15,7 @@ apt update
 apt upgrade -y
 
 # Install Pre-requisite
-apt install build-essential git libx11-dev libxft-dev libxinerama-dev xorg dmenu unzip lxpolkit x11-xserver-utils unzip wget pulseaudio alacritty pavucontrol xdg-user-dirs
+apt install -y thunar build-essential git libx11-dev libxft-dev libxinerama-dev xorg dmenu unzip lxpolkit x11-xserver-utils unzip wget pulseaudio alacritty pavucontrol xdg-user-dirs
 
 
 # Install Dwm
@@ -23,31 +23,29 @@ git clone https://github.com/sebabellucci/debian-dwm.git
 cd debian-dwm
 
 # Making .config and Moving config files and background to Pictures
-cd $builddir
 mkdir -p /home/$username/.config
-mkdir -p /home/$username/Pictures
-mkdir -p /usr/share/sddm/themes
+mkdir -p /home/$username/Pictures/Wallpapers
+touch /home/$username/.xinitrc
 cp .xinitrc /home/$username
 cp -R dotfiles/dotconfig/* /home/$username/.config/
-cp dotfiles/Wallpapers/* /home/$username/Pictures/
+cp dotfiles/Wallpapers/* /home/$username/Pictures/Wallpapers/
 chown -R $username:$username /home/$username
-cd..
+cd ..
 
 # Install Dwm
 git clone https://github.com/Ferchupessoadev/dwm.git
 cd dwm
 make clean install
 mkdir -p ~/.local/share/dwm
-cd..
 cp -r autostart.sh ~/.local/share/dwm/autostart.sh
 cp -r dwmbar ~/.config/dwmbar
-cd..
+cd ..
 
 #Install ST
 git clone https://github.com/Ferchupessoadev/st.git
 cd st
 make install clean
-cd..
+cd ..
 
 # Installing fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
@@ -60,4 +58,4 @@ fc-cache -vf
 rm ./JetBrainsMono.zipip
 
 # Installing software adtional
-apt install arandr nitrogen chromium neofetch 
+apt install -y arandr nitrogen chromium neofetch 
